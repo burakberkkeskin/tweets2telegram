9ebbb241c8bc2d754bc42c7234ac0142333f5e53
safderun <burakberkkeskin@gmail.com>
2021-10-08 12:48:50 +0300

    improved messages

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {
   consumer_secret,
   access_token,
   access_token_secret,
-  chat_id
+  chat_id,
 } = require("./secret.json");
 const Twitter = require("twitter");
 
@@ -21,24 +21,26 @@ const client = new Twitter({
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 async function getLastTweet() {
-  var lastTweet = "";
+  var savedTweet = "";
 
   while (true) {
     var params = { list_id: "1432403151492091911", tweet_mode: "extended" };
     client.get(
-        "lists/statuses.json",
-        params,
-        function (error, tweets, response) {
-          if (!error && tweets.length > 0) {
-            if (tweets[0].full_text != lastTweet) {
-              sendMessage(tweets[0].full_text);
-              lastTweet = tweets[0].full_text;
-            }
-          } else if (error) {
-            console.log(error);
+      "lists/statuses.json",
+      params,
+      function (error, tweets, response) {
+        if (!error && tweets.length > 0) {
+          const lastTweet = tweets[0]
+          if (lastTweet.full_text != savedTweet) {
+            const message = createMessage(titleCase(lastTweet.user.name), lastTweet.full_text, lastTweet.id_str)
+            sendMessage(message);
+            savedTweet = lastTweet.full_text;
           }
+        } else if (error) {
+          console.log(error);
         }
-      );
+      }
+    );
 
     await delay(5000);
   }
@@ -48,9 +50,25 @@ function sendMessage(text) {
   slimbot.sendMessage(chat_id, text);
 }
 
+function createMessage(tweetOwner, tweetText, tweetId){
+  return tweetOwner+'\n\n'+tweetText+'\n\nTweet Source: https://twitter.com/twitter/statuses/'+tweetId
+}
+
+function titleCase(str) {
+  var splitStr = str.toLowerCase().split(' ');
+  for (var i = 0; i < splitStr.length; i++) {
+      // You do not need to check if i is larger than splitStr length, as your for does that for you
+      // Assign it back to the array
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+  }
+  // Directly return the joined string
+  return splitStr.join(' '); 
+}
+
 function main() {
-console.log('Kripto Para Listesi Dinlemeye Alındı...');
+  console.log("Kripto Para Listesi Dinlemeye Alındı...");
   getLastTweet();
 }
 
 main();
+
